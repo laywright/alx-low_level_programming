9038ec6f3aa52dbc52d62d91e757d49a8edecb14
Alex <laywright96@gmail.com>
2023-02-21 11:08:45 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point for a program to print _putchar
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	char ptchar[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+		_putchar(ptchar[i]);
+	_putchar('\n');
+
+	return (0);
+}
