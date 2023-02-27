54175a574e0e6bee89991c0e32e016e97418ebde
Alex <laywright96@gmail.com>
2023-02-27 06:27:14 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - Program that reverses a string
+ * @s: Pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i, j, count = 0, temp;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(s + j);
+			*(s + j) = *(s + (j - 1));
+			*(s + (j - 1)) = temp;
+		}
+	}
+}
