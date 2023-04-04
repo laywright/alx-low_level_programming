61565bda56280e0e2937bbae89ef165fc888dd5e
laywright <laywright96@gmail.com>
2023-04-04 14:14:35 -0700

    task 11

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listp - frees the linked list
+ * free_listp - frees a linked list
  * @head: head of a list.
  *
  * Return: no return.
@@ -27,7 +27,7 @@ void free_listp(listp_t **head)
  * print_listint_safe - prints a linked list.
  * @head: head of a list.
  *
- * Return: returns number of nodes in the list.
+ * Return: number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
